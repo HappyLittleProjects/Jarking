30f15ff8d39a18686848a340c6674ef5cf2ea643
Linda Lau <imawalrus22@gmail.com>
2023-03-09 18:00:26 -0700

    Add project files.

---
+++ Main/Jar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    internal class Jar
+    {
+        // keeps track of all jars that are created [ should be put into db ]
+        private static Dictionary<string, Jar> jar = new Dictionary<string, Jar>();
+
+        public string name;             // name of jar
+        public string description;      // kinds of thoughts in the jar
+
+        private string id;              // jar's id
+        
+        // thought storage
+        private List<string> thought = new List<string>();
+
+        /// <summary>
+        /// Create a jar; Defines name and description and creates id for the jar.
+        /// </summary>
+        /// <param name="name"></param> Name of jar
+        /// <param name="description"></param>  Describe what jar is for
+        public Jar(string name, string description = "") 
+        { 
+            this.name = name;
+            this.description = description;
+            this.id = $"{name}{jar.Count}";
+            jar.Add(this.id, this );
+        }
+
+        /// <summary>
+        /// Add thought to the jar
+        /// </summary>
+        /// <param name="thought"></param>
+        public void Add(string thought)
+        {
+            this.thought.Add(thought);
+        }
+
+        /// <summary>
+        /// Returns a random string that is stored in the thoughts list.
+        /// </summary>
+        /// <returns></returns>
+        public string Pull()
+        {
+            int num = new Random().Next(0, this.thought.Count);
+            return this.thought[num];
+        }
+
+        /// <summary>
+        /// Returns a jar with the given id. This function is meant for when jars
+        /// are shared.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Jar GetJar(string id)
+        {
+            return jar[id];
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,27 @@
+﻿using Main;
+
+Jar jar = new Jar("test jar", "testing jars");
+
+jar.Add("thought 1");
+jar.Add("thought 2");
+jar.Add("thought 3");
+jar.Add("thought 4");
+jar.Add("thought 5");
+jar.Add("thought 6");
+jar.Add("thought 7");
+jar.Add("thought 8");
+jar.Add("thought 9");
+jar.Add("thought 10");
+
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+Console.WriteLine(jar.Pull());
+
+Console.ReadLine();
